86ff900b3fce4f1638e6027e26884cc0001d46a7
Andres Sosa <asosa@nexsense.com>
2015-04-03 12:11:39 -0600

    Working on the InvoiceItems stuff

--- source/Databases/WiseCrm/STORED_PROCEDURES/CUST/custAE_InventoryItemsSycnWithMsAccountEquipmentInstalled.sql
+++ source/Databases/WiseCrm/STORED_PROCEDURES/CUST/custAE_InventoryItemsSycnWithMsAccountEquipmentInstalled.sql
@@ -44,30 +44,47 @@ BEGIN
 	/** SET NO COUNTING */
 	SET NOCOUNT ON
 
-	/** Check that the account has not been submitted to CS. */
+	/** Check that the account has not been submitted to CS. 
 	IF(EXISTS(SELECT AccountID FROM [dbo].[MS_AccountSalesInformations] WHERE (AccountID = @AccountID) AND (InstallDate IS NOT NULL)))
 	BEGIN
 		RETURN;
 	END
+	*/
 
 	/** DECLARATIONS */
-	DECLARE @EquipmentId VARCHAR(50)
+	DECLARE @AccountEquipmentID BIGINT
+		, @EquipmentId VARCHAR(50)
 		, @ActualPoints FLOAT
 		, @BarcodeId NVARCHAR(25)
+		, @ItemSKU VARCHAR(20)
 		, @InvoiceItemID BIGINT
 	
 	BEGIN TRY
+		BEGIN TRANSACTION
+
 		/** Create a cursor */
 		DECLARE accountEqCursor CURSOR FOR
-		SELECT EquipmentId, ActualPoints, BarcodeId FROM [dbo].[MS_AccountEquipment] WHERE (AccountId = @AccountID) AND (IsActive = 1 AND IsDeleted = 0);
+		SELECT MSAE.AccountEquipmentID, MSAE.EquipmentId, MSAE.ActualPoints, MSAE.BarcodeId , AEI.ItemSKU
+		FROM 
+			[dbo].[MS_AccountEquipment] AS MSAE WITH (NOLOCK)
+			INNER JOIN [dbo].[AE_Items] AS AEI WITH (NOLOCK)
+			ON
+				(AEI.ItemID = MSAE.EquipmentId)
+		WHERE
+			(AccountId = @AccountID) 
+			AND (InvoiceItemID IS NULL) 
+			AND (MSAE.IsActive = 1 AND MSAE.IsDeleted = 0);
 
 		OPEN accountEqCursor;
 
 		FETCH NEXT FROM accountEqCursor
-		INTO @EquipmentId, @ActualPoints, @BarcodeId
+		INTO @AccountEquipmentID, @EquipmentId, @ActualPoints, @BarcodeId, @ItemSKU
 
 		WHILE(@@FETCH_STATUS = 0)
 		BEGIN
+			/** Initialize */
+			SET @InvoiceItemID = NULL;
+
 			/** Find a matching SKU with no barcode ID */
 			SELECT TOP 1 
 				@InvoiceItemID = InvoiceItemID 
@@ -84,16 +101,29 @@ BEGIN
 				AND (AEI.IsActive = 1 AND AEI.IsDeleted = 0)
 				AND (AEII.IsActive = 1 AND AEII.IsDeleted = 0)
 
+			/** Check that there is an invoiceitem otherwise create one. */
+			IF (@InvoiceItemID IS NULL)
+			BEGIN
+				PRINT 'No Invoice Item found for EquipmentID: ' + @EquipmentId + ',  AccountEquipmentID: ' + CAST(@AccountEquipmentID AS VARCHAR) + ', and Part #: ' + @ItemSKU;
+			END
+			ELSE
+			BEGIN
+				PRINT 'Invoice Item ' + CAST(@InvoiceItemID AS VARCHAR) + ' for EquipmentID: ' + @EquipmentId + ', AccountEquipmentID: ' + CAST(@AccountEquipmentID AS VARCHAR) + ', and Part #: ' + @ItemSKU;
+			END
+
 			/** Move to the next item. */
 			FETCH NEXT FROM accountEqCursor
-			INTO @EquipmentId, @ActualPoints, @BarcodeId
+			INTO @AccountEquipmentID, @EquipmentId, @ActualPoints, @BarcodeId, @ItemSKU
 		END
 
 		CLOSE accountEqCursor;
 		DEALLOCATE accountEqCursor;
 
+		ROLLBACK TRANSACTION
 	END TRY
 	BEGIN CATCH
+		ROLLBACK TRANSACTION
+
 		EXEC dbo.wiseSP_ExceptionsThrown;
 		RETURN;
 	END CATCH
@@ -103,4 +133,4 @@ GO
 GRANT EXEC ON dbo.custAE_InventoryItemsSycnWithMsAccountEquipmentInstalled TO PUBLIC
 GO
 
-/** EXEC dbo.custAE_InventoryItemsSycnWithMsAccountEquipmentInstalled */
+/** EXEC dbo.custAE_InventoryItemsSycnWithMsAccountEquipmentInstalled 150927 */
