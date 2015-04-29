7c86228cf5506868afffd95ae2cc372433f556bf
Aaron Shumway <aclshumway@gmail.com>
2015-04-29 15:54:40 -0600

    started admin end points

---
+++ source/Data/NXS.Data.AuthenticationControl/Extensions/AC_GroupActionTableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace NXS.Data.AuthenticationControl
+{
+	using AR = AC_GroupAction;
+	using ARCollection = IEnumerable<AC_GroupAction>;
+	using ARTable = AuthControlDb.AC_GroupActionTable;
+	public static class AC_GroupActionTableExtensions
+	{
+		public static async Task InsertAsync(this ARTable tbl, AR item, string gpEmployeeId)
+		{
+			item.ModifiedOn = item.CreatedOn = DateTime.UtcNow.RoundToSqlDateTime();
+			item.ModifiedBy = item.CreatedBy = gpEmployeeId;
+			item.ID = await tbl.InsertAsync(item).ConfigureAwait(false);
+		}
+		public static async Task UpdateAsync(this ARTable tbl, Snapshotter.Snapshot<AR> snapShot, string gpEmployeeId)
+		{
+			var item = snapShot.Value;
+			item.ModifiedOn = DateTime.UtcNow.RoundToSqlDateTime();
+			item.ModifiedBy = gpEmployeeId;
+			await tbl.UpdateAsync(item.ID, snapShot.Diff()).ConfigureAwait(false);
+		}
+	}
+}
